16ce7b09d016ad3fc9dd94d2fe0e2499183f2a1d
Jimmy Briggs <jimbrig2011@outlook.com>
2021-04-28 20:22:11 -0400

    cleanup and update gcloud

---
+++ gcloud/install_gcloudsdk.ps1
@@ -0,0 +1,3 @@
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe", "$env:Temp\GoogleCloudSDKInstaller.exe")
+
+& $env:Temp\GoogleCloudSDKInstaller.exe
