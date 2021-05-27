12785bff105aa4d1ca9f96f31c597eab37b3add7
Jimmy Briggs <jimbrig2011@outlook.com>
2021-05-27 18:57:57 -0400

    add postgresql installation script for ubuntu

---
+++ scripts/install-postgresql-ubuntu.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt install postgresql postgresql-contrib
+
+psql --version
+
+# sudo service postgresql status for checking the status of your database.
+# sudo service postgresql start to start running your database.
+# sudo service postgresql stop to stop running your database.
