77177e91db4cd9a0b0d307b88e8158f76d7ba91f
Jimmy Briggs <jimbrig2011@outlook.com>
2021-05-27 18:33:58 -0400

    gcloudsdk install script

---
+++ scripts/install_gcloudsdk.ps1
@@ -0,0 +1,4 @@
+(New-Object Net.WebClient).DownloadFile("https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe", "$env:Temp\GoogleCloudSDKInstaller.exe")
+
+& $env:Temp\GoogleCloudSDKInstaller.exe
+      
