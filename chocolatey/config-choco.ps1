7d5604cb12c78fbc2e792fb6bbaaa0db71e49f36
Jimmy Briggs <jimbrig1993@outlook.com>
2021-07-02 23:51:28 -0400

    update choco configuration and installation scripts

--- chocolatey/config-choco.ps1
+++ chocolatey/config-choco.ps1
@@ -1,10 +1,38 @@
 refreshenv
 
 # configure chocolatey
+Write-Host "Installing internal chocolatey packages"  -ForegroundColor Yellow
 choco upgrade chocolatey choco-cleaner choco-package-list-backup -y
+
+if (!(Test-Path -Path $PROFILE)) {
+  Write-Host "Creating Powershell Profile" -ForegroundColor Green
+  New-Item -ItemType File -Path $PROFILE -Force
+
+  Add-Content -Path $profile -Value '# chocolatey profile
+  $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+  if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+  }'
+  Write-Host "Added Chocolatey to PowerShell Profile" -ForegroundColor Green
+}
+
+# features
+Write-Host "Configuring Chocolatey's Settings and Features..." -ForegroundColor Yellow
+
 choco feature enable -n allowGlobalConfirmation
 choco config set cacheLocation $env:TEMP
 choco feature enable -n logEnvironmentValues
 choco feature enable -n virusCheck
+choco config set virusScannerType VirusTotal
 choco feature enable -n useRememberedArgumentsForUpgrades
 choco feature enable -n removePackageInformationOnUninstall
+choco feature enable -n allowEmptyChecksums
+
+
+Write-Host "Done. Current feature set is: " -ForegroundColor Green
+choco feature list
+
+refreshenv
+. $profile
+refreshenv
+
--- chocolatey/install-choco.ps1
+++ chocolatey/install-choco.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Unrestricted
 Set-ExecutionPolicy Unrestricted -scope CurrentUser
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 refreshenv
