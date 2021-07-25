7ff9d68ae9ba3f2f5445d024282a9880a3f94930
Jimmy Briggs <jimbrig1993@outlook.com>
2021-07-25 16:14:17 -0400

    updated choco-installs.ps1 script

---
+++ setup/scripts/choco-installs.ps1
@@ -0,0 +1,84 @@
+# chocolatey and extensions
+choco upgrade -y chocolatey `
+  chocolatey-core.extension `
+  choco-cleaner `
+  chocolatey-misc-helpers.extension `
+  chocolatey-windowsupdate.extension `
+  choco-package-list-backup `
+  choco-protocol-support `
+  choco-shortcuts-winconfig `
+  instchoco `
+
+# utility
+autohotkey.install `
+  7zip.install `
+  7zip.commandline `
+  LinkShellExtension `
+  qttabbar `
+  rapidee `
+  rufus `
+  lepton `
+  pandoc `
+  winaero-tweaker `
+  t-clock `
+  Sudo `
+  uninstalltool `
+
+# productivity
+evernote `
+  todoist `
+  obsidian `
+
+# system
+DotNet4.5.2 `
+  dotnetfix `
+  KB2919355 `
+  KB2919442 `
+  KB2999226 `
+  KB3033929 `
+  KB3035131 `
+  vcredist140 `
+  vcredist2010 `
+  vcredist2013 `
+  vcredist2015 `
+  vcredist2017 `
+
+# devtools
+git.install `
+  cmake.install `
+  Ghostscript `
+  Ghostscript.app `
+  heroku-cli `
+  qpdf `
+  openjdk `
+  postman `
+  sqlitebrowser.install `
+  terraform `
+  tortoisegit `
+  typora `
+  vscode-insiders `
+  vscode `
+  docker-desktop `
+  yori `
+  python `
+  kubernetes-helm `
+  minikube `
+  gcloudsdk `
+  azure-cli `
+  # postgresql
+  pgadmin4 `
+  pgcli `
+  # node version manager
+  nvm `
+
+
+# comms
+discord.install `
+  slack.install `
+  mailbird `
+
+# PDF
+sumatrapdf.install `
+
+# video
+vlc
