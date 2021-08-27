b0da93890b0411faaa2cb92ce8f59bb2dddd73dc
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-08-27 13:20:29 -0400

    update s-cli install script

--- s-cli/install-s-cli.ps1
+++ s-cli/install-s-cli.ps1
@@ -1,19 +1,24 @@
 # install from github
 # example: Install-Github "user/repo" "*.exe" "program-name"
-function Install-Github($repo, $pattern, $name) {
-  Save-fromGH $repo $pattern $name
-  $extension = Get-
-  $extension = $pattern.Replace("*", "")
-  $installfile = $name + $extension
-  $installpath = "$env:USERPROFILE\Downloads\" + $installfile
-  Start-Process $installpath
-}
+# function Install-Github($repo, $pattern, $name) {
+#   Save-fromGH $repo $pattern $name
+#   $extension = Get-
+#   $extension = $pattern.Replace("*", "")
+#   $installfile = $name + $extension
+#   $installpath = "$env:USERPROFILE\Downloads\" + $installfile
+#   Start-Process $installpath
+# }
 
-Install-Github -repo "zquestz/s" -pattern "*windows_amd64.zip" -name "s-cli"
+# Install-Github -repo "zquestz/s" -pattern "*windows_amd64.zip" -name "s-cli"
+
+lastversion zquestz/s --format assets --filter "windows_amd64" -d "Downloads/s-cli.zip"
 
 Import-Module Microsoft.PowerShell.Archive
 mkdir c:\tools\s-cli
-Expand-Archive "$HOME\Downloads\s-cliwindows_amd64.zip" -DestinationPath "C:\tools\s-cli"
+Expand-Archive "$HOME\Downloads\s-cli.zip" -DestinationPath "C:\tools"
 
-mkdir c:\bin
-Copy-Item "C:\tools\s-cli\s.exe" "c:\bin\*"
+if (-not (test-path "C:\bin")) mkdir c:\bin
+
+$subdir = Get-ChildItem "C:\tools"
+$exepath = "C:\tools\s-cli\" + $subdir.Name + "\s.exe"
+Copy-Item $exepath "c:\bin\"
