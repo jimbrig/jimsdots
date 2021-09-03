d2e9d9c81eb4f72dc9ad50ee63f6e2b52e0abf8f
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-09-03 18:25:59 -0400

    bugfix: MSI scripts for SSH

--- ssh/MSI/MSI-link-ssh-keys.ps1
+++ ssh/MSI/MSI-link-ssh-keys.ps1
@@ -3,10 +3,10 @@ Set-Location ~/.dotfiles/ssh
 # git-crypt unlock
 
 # link keys into ~/.ssh
-if (-not (test-path "$HOME\.ssh")) mkdir "$HOME\.ssh"
-New-Item -Path "$HOME\.ssh\config" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\config"
-New-Item -Path "$HOME\.ssh\known_hosts" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\known_hosts"
-New-Item -Path "$HOME\.ssh\id_rsa" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\MSI_id_rsa"
-New-Item -Path "$HOME\.ssh\id_rsa.pub" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\MSI_id_rsa.pub"
+if (-not (test-path "$HOME\.ssh")) { mkdir "$HOME\.ssh" }
+New-Item -Path "$HOME\.ssh\config" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\config" -Force
+New-Item -Path "$HOME\.ssh\known_hosts" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\known_hosts" -Force
+New-Item -Path "$HOME\.ssh\id_rsa" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\MSI_id_rsa" -Force
+New-Item -Path "$HOME\.ssh\id_rsa.pub" -ItemType SymbolicLink -Value "$HOME\.dotfiles\ssh\MSI\MSI_id_rsa.pub" -Force
 
 explorer.exe "$HOME\.ssh"
