412358d67e9cdd60dfc3bab272db7d40b5583387
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-09-04 03:47:01 -0400

    Create config-winget.ps1

---
+++ winget/config-winget.ps1
@@ -0,0 +1,15 @@
+# Specify some variables:
+$wingetsettingsdir = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState"
+$wingetsettingspath = "$wingetsettingsdir\settings.json"
+$backuppath = "$wingetsettingsdir\settings.json.bak"
+$dotfilespath = "~/.dotfiles/winget/settings.json"
+
+# Rename current settings as a backup (if they exist):
+if (Test-Path $wingetsettingspath) { Rename-Item $wingetsettingspath $backuppath }
+
+# Copy settings.json to winget settings directory:
+Copy-Item $dotfilespath $wingetsettingsdir
+
+# Refresh and test
+refreshenv
+winget settings
