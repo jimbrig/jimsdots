d66b93f41f7f7f676c559bd74a92956da588b10d
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-09-03 21:51:45 -0400

    fix office installation procedure

--- office/install/install.ps1
+++ office/install/install.ps1
@@ -1,1 +1,2 @@
-.\setup.exe /configure configuration-Office365-x64.xml
+Copy-Item "$HOME\OneDrive\Backup\Programs\Office\OfficeSetup.exe" -Destination .
+.\OfficeSetup.exe /configure configuration-Office365-x64.xml
