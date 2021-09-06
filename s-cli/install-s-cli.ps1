08cd580bcbe07420b19631961c0777c76b3eba4e
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-09-06 16:07:43 -0400

    improve s-cli download script

--- s-cli/install-s-cli.ps1
+++ s-cli/install-s-cli.ps1
@@ -1,3 +1,16 @@
+# Download ZIP Archive:
+lastversion zquestz/s --format assets --filter "windows_amd64" -d "Downloads/s-cli.zip"
+
+# Unzip to C:\tools:
+New-Item -ItemType Directory -Path "C:\tools\s-cli" -Force
+Expand-Archive "$HOME\Downloads\s-cli.zip" -DestinationPath "C:\tools"
+
+# Add s.exe to C:\bin
+if (-not (test-path "C:\bin")) { New-Item -ItemType Directory "C:\bin" }
+$subdir = Get-ChildItem "C:\tools"
+$exepath = "C:\tools\s-cli\" + $subdir.Name + "\s.exe"
+Copy-Item $exepath "c:\bin\"
+
 # install from github
 # example: Install-Github "user/repo" "*.exe" "program-name"
 # function Install-Github($repo, $pattern, $name) {
@@ -10,15 +23,3 @@
 # }
 
 # Install-Github -repo "zquestz/s" -pattern "*windows_amd64.zip" -name "s-cli"
-
-lastversion zquestz/s --format assets --filter "windows_amd64" -d "Downloads/s-cli.zip"
-
-Import-Module Microsoft.PowerShell.Archive
-mkdir c:\tools\s-cli
-Expand-Archive "$HOME\Downloads\s-cli.zip" -DestinationPath "C:\tools"
-
-if (-not (test-path "C:\bin")) mkdir c:\bin
-
-$subdir = Get-ChildItem "C:\tools"
-$exepath = "C:\tools\s-cli\" + $subdir.Name + "\s.exe"
-Copy-Item $exepath "c:\bin\"
