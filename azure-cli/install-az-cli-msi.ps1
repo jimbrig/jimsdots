7d22a7d812f45a8f659da26fafbba961346bc030
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-10-04 18:39:27 -0400

    add new az-cli installation script

---
+++ azure-cli/install-az-cli-msi.ps1
@@ -0,0 +1,12 @@
+# See https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-powershell
+
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
+# once complete
+refreshenv
+
+# login
+az login
+
+# to upgrade
+# az upgrade
