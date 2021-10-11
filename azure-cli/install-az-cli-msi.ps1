a7918a6bf76d8c98b86e3865de0ca95a1de965c8
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-10-10 23:43:43 -0400

    refactor: use approved PowerShell verbage

--- azure-cli/install-az-cli-msi.ps1
+++ azure-cli/install-az-cli-msi.ps1
@@ -1,6 +1,6 @@
 # See https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-powershell
 
-$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; Remove-Item .\AzureCLI.msi
 
 # once complete
 refreshenv
